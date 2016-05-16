50b6e6e42b1ce00d9451f942b27898e43a204130
Valery Ryaboshapko <valera@creator.su>
2016-05-16 08:48:44 +0300

    solved problem with selecting of dev database

--- accountant/migrations/0002_test_data.py
+++ accountant/migrations/0002_test_data.py
@@ -9,6 +9,7 @@ from accountant.models import Account
 
 
 def create_accounts(apps, schema_editor):
+    account_model = apps.get_model('accountant', 'Account')
     test_accounts = (
         {'title': 'Кошелёк', 'type': Account.ACCOUNT, 'value': Money(0, RUB)},
         {'title': 'Tinkoff RUB', 'type': Account.ACCOUNT, 'value': Money(0, RUB)},
@@ -16,20 +17,20 @@ def create_accounts(apps, schema_editor):
         {'title': 'Tinkoff EUR', 'type': Account.ACCOUNT, 'value': Money(0, EUR)},
     )
     for account in test_accounts:
-        Account.objects.create(**account)
+        account_model.objects.create(**account)
 
     test_incomes = (
         {'title': 'Белая зарплата', 'type': Account.INCOME, 'value': Money(0, RUB)},
         {'title': 'Серая зарплата', 'type': Account.INCOME, 'value': Money(0, RUB)},
     )
     for income in test_incomes:
-        Account.objects.create(**income)
-    interest_rates = Account.objects.create(
+        account_model.objects.create(**income)
+    interest_rates = account_model.objects.create(
         title='Проценты по вкладам',
         type=Account.INCOME,
         value=Money(0, RUB)
     )
-    Account.objects.create(
+    account_model.objects.create(
         title='Tinkoff RUB',
         type=Account.INCOME,
         value=Money(0, RUB),
--- pre_dev_run.py
+++ pre_dev_run.py
@@ -1,27 +1,31 @@
 import os
 import django
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'frekenbok.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'frekenbok.settings.dev'
 django.setup()
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core import management
 
-# This tiny script prepares dev environment for start it
-# 1. removes old DB
+# This tiny script prepares dev environment for start. It...
+# 1. ...removes old DB...
 try:
-    os.unlink('db.sqlite3')
-    print('Dev database removed')
+    os.unlink(settings.DATABASES['default']['NAME'])
+    print('Dev database ({}) removed'.format(settings.DATABASES['default']['NAME']))
 except FileNotFoundError:
     print('Dev database not found')
 except Exception as e:
     print('Can\'t delete dev database: {}'.format(e))
     exit(1)
-# 2. makes migrations
+# 2. ...migrates...
 management.call_command('migrate')
-# 3. compiles PO files
+# 3. ...compiles PO files...
 management.call_command('compilemessages')
-# 4. creates superuser
-User.objects.create_superuser(username='valera', password='vena89portae', email='valera@creator.su')
+# 4. ...creates superuser
+User.objects.create_superuser(
+    username='valera',
+    password='vena89portae',
+    email='valera@creator.su')
 # ...
 # PROFIT!!!1
