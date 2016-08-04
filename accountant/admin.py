ebce40d1331d195515ffb267d519ec7083e2ab49
Valery Ryaboshapko <valera@creator.su>
2016-08-04 03:43:33 +0300

    added sheaves to admin

--- accountant/admin.py
+++ accountant/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Account, Transaction, Document, Invoice
+from .models import Account, Sheaf, Transaction, Document, Invoice
 
 admin.site.register(Account)
+admin.site.register(Sheaf)
 admin.site.register(Transaction)
 admin.site.register(Document)
 admin.site.register(Invoice)
