1f7c10cd10a4fecea15871032519ef9fced235bf
Valery Ryaboshapko <vr@exante.eu>
2016-10-31 12:51:50 +0300

    trying to deal with tests for views

--- accountant/tests/test_views.py
+++ accountant/tests/test_views.py
@@ -1,5 +1,19 @@
 from django.test import TestCase
 
+from .test_data import prepare_test_data
+
+from accountant.models import Account
+from accountant.views import DashboardView
 
 class DashboardViewTestCase(TestCase):
-    pass
+    def setUp(self):
+        prepare_test_data(self)
+        self.view = DashboardView()
+
+    def test_get_queryset(self):
+        for item in self.view.get_queryset():
+            self.assertEqual(item.type, Account.ACCOUNT)
+
+    def test_get_context_data_mixin_included(self):
+        context = self.view.get_context_data()
+        self.assertTrue(context['accountant_app'])
