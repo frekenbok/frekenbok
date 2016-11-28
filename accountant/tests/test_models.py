3a599cccbaf3b900c282d1ba9c0082dee547587d
Valery Ryaboshapko <vr@exante.eu>
2016-11-28 03:28:50 +0300

    removed some crap from models tests

--- accountant/tests/test_models.py
+++ accountant/tests/test_models.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from django.conf import settings
 
 from django.db.utils import IntegrityError
-from autofixture.base import CreateInstanceError
+from autofixture.generators import ChoicesGenerator
 
 from autofixture import AutoFixture
-from moneyed import Decimal, SAR
+from moneyed import Decimal, RUB, EUR, USD, SAR, JPY
 
 from accountant.models import Account, Sheaf
 
@@ -15,17 +15,16 @@ class AccountTestCase(TestCase):
     def setUpTestData(cls):
         cls.accounts = AutoFixture(Account).create(1)
         for account in cls.accounts:
-            for i in range(3):
-                try:
-                    AutoFixture(
-                        Sheaf,
-                        field_values={'account': account}
-                    ).create_one()
-                except CreateInstanceError:
-                    continue
+            AutoFixture(
+                Sheaf,
+                field_values={
+                    'account': account,
+                    'currency': ChoicesGenerator(values=(RUB, EUR, USD))
+                }
+            ).create(3)
 
     def test_sorted_sheaves(self):
-        currencies = [i.currency for i in self.accounts[0].sheaves.all()]
+        currencies = [i.currency for i in self.accounts[0].sorted_sheaves]
 
         self.assertEqual(currencies[0], settings.BASE_CURRENCY)
 
@@ -40,21 +39,20 @@ class SheafTestCase(TestCase):
     def setUpTestData(cls):
         cls.accounts = AutoFixture(Account).create(1)
         for account in cls.accounts:
-            for i in range(3):
-                try:
-                    AutoFixture(
-                        Sheaf,
-                        field_values={'account': account}
-                    ).create_one()
-                except CreateInstanceError:
-                    continue
+            AutoFixture(
+                Sheaf,
+                field_values={
+                    'account': account,
+                    'currency': ChoicesGenerator(values=(RUB, EUR, USD, SAR))
+                }
+            ).create(3)
 
     def test_one_sheaf_currency_for_per_account(self):
         Sheaf.objects.create(account=self.accounts[0],
                              amount=Decimal('34532.12'),
-                             currency=SAR)
+                             currency=JPY)
 
         with self.assertRaises(IntegrityError):
             Sheaf.objects.create(account=self.accounts[0],
                                  amount=Decimal('932.12'),
-                                 currency=SAR)
+                                 currency=JPY)
