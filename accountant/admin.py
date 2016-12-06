1b19d06c72535500eacbbda112b9a4aff3436c86
Valery Ryaboshapko <vr@exante.eu>
2016-12-06 04:28:31 +0300

    improve admin

--- accountant/admin.py
+++ accountant/admin.py
@@ -8,9 +8,26 @@ from .models import Account, Sheaf, Transaction, Document, Invoice
 class AccountAdmin(TreeAdmin):
     form = movenodeform_factory(Account)
 
-
 admin.site.register(Account, AccountAdmin)
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('account', 'amount', 'currency', 'date')
+
+admin.site.register(Transaction, TransactionAdmin)
+
+
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 2
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'comment')
+    inlines = (TransactionInline,)
+
+admin.site.register(Invoice, InvoiceAdmin)
+
+
 admin.site.register(Sheaf)
-admin.site.register(Transaction)
 admin.site.register(Document)
-admin.site.register(Invoice)
