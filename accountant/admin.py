07f0c920b98ecc65386284c84d14e4d221af9c4b
Valery Ryaboshapko <vr@exante.eu>
2016-12-06 03:47:52 +0300

    improve admin

--- accountant/admin.py
+++ accountant/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
 from .models import Account, Sheaf, Transaction, Document, Invoice
 
-admin.site.register(Account)
+
+class AccountAdmin(TreeAdmin):
+    form = movenodeform_factory(Account)
+
+
+admin.site.register(Account, AccountAdmin)
 admin.site.register(Sheaf)
 admin.site.register(Transaction)
 admin.site.register(Document)
