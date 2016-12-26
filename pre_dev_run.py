94edfb5418e7b8b34f8490ff09fc37e0aedc245c
Valery Ryaboshapko <vr@exante.eu>
2016-12-26 05:46:21 +0300

    test data for dev run

--- pre_dev_run.py
+++ pre_dev_run.py
@@ -10,6 +10,14 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core import management
 
+from accountant.tests.test_data import add_test_data
+
+
+class DevData:
+    def __new__(cls):
+        add_test_data(cls)
+
+
 # This tiny script prepares dev environment for start. It...
 # 1. ...removes old DB...
 try:
@@ -29,5 +37,7 @@ User.objects.create_superuser(
     username='valera',
     password='vena89portae',
     email='valera@creator.su')
+# 5. Import test data
+DevData()
 # ...
 # PROFIT!!!1
