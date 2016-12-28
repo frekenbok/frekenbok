410e2e8d3de761e8cfce4a05c5c2496a2a49ce04
Valery Ryaboshapko <vr@exante.eu>
2016-12-28 04:10:21 +0300

    added medai root and media uri

--- frekenbok/settings/dev.py
+++ frekenbok/settings/dev.py
@@ -6,9 +6,12 @@ SECRET_KEY = '_23n6hl)*4z)s+bn*7o7wk(ce@v2e7@#l=iv%4%5=4ua!5@%+='
 
 DEBUG = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../development.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', 'development.sqlite3'),
     }
 }
