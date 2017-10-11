73816492d90147b7e1df5ba939dab095c3d3563a
Valery Ryaboshapko <vr@exante.eu>
2017-10-11 12:42:07 +0300

    some updates

--- accountant/views/statement_import_view.py
+++ accountant/views/statement_import_view.py
@@ -1,7 +1,10 @@
 import logging
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
 logger = logging.getLogger(__name__)
 
 
-class StatementImportView:
-    pass
+class StatementImportView(LoginRequiredMixin, TemplateView):
+    template_name = 'accountant/statement_import.html'
