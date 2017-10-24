4a53c47eff829e4920e3de54322394f8f035d28c
Valery Ryaboshapko <vr@exante.eu>
2017-10-25 01:25:06 +0300

    some more updates

--- accountant/views/statement_import_view.py
+++ accountant/views/statement_import_view.py
@@ -1,10 +1,36 @@
 import logging
 
+from django.apps import apps
+from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, JsonResponse, HttpResponseBadRequest
 from django.views.generic import TemplateView
+
+from accountant.models import Document
 
 logger = logging.getLogger(__name__)
 
 
 class StatementImportView(LoginRequiredMixin, TemplateView):
     template_name = 'accountant/statement_import.html'
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        statement = request.FILES.get('statement')
+        if statement:
+            doc = Document.objects.create(
+                file=statement,
+                invoice=None,
+                description=""
+            )
+            return JsonResponse(doc.json())
+
+        try:
+            document_id = int(request.POST.get('document'))
+        except ValueError:
+            return HttpResponseBadRequest(
+                'Can\'t parse {} as document id'.format(
+                    request.POST.get('document')))
+
+        settings.INSTALLED_APPS
+
+        apps
