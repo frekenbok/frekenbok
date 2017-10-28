36452c61927f50ac062e5a50a89011ded1650458
Valery Ryaboshapko <vr@exante.eu>
2017-10-28 04:20:30 +0300

    trying to fix MySQL tests

--- frekenbok/settings/test_mysql_travis.py
+++ frekenbok/settings/test_mysql_travis.py
@@ -2,3 +2,4 @@ from frekenbok.settings.test_mysql import *
 
 DATABASES['default']['USER'] = 'root'
 DATABASES['default']['PASSWORD'] = ''
+DATABASES['default']['OPTIONS'] = {'init_command': 'SET names "utf8";'}
